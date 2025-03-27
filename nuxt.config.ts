15eed6ac94b5cb24048c1156965909a1bbf92e2d
gowtham-009 <139350157+gowtham-009@users.noreply.github.com>
2025-03-27 12:24:06 +0530

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,17 @@
 import * as path from "path";
 
 export default defineNuxtConfig({
+    app: {
+        head: {
+          title: 'NKYC', // default fallback title
+          htmlAttrs: {
+            lang: 'en',
+          },
+          link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+          ]
+        }
+      },
     modules: [ "@primevue/nuxt-module"],
     css: ['@/assets/styles/tailwind.css', '@/assets/styles/base.css'],
     primevue: {
