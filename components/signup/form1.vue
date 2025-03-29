46dbb81979ee9e8dd8e839f5a14f209be65a275a
gowtham-009 <139350157+gowtham-009@users.noreply.github.com>
2025-03-29 10:43:26 +0530

    chan

--- components/signup/form1.vue
+++ components/signup/form1.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="primary_color">
     <div class="w-full p-2 primary_color"
-      :style="{ height: deviceHeight * 0.40 + 'px' }">
+      :style="{ height: deviceHeight * 0.30 + 'px' }">
      <div class="w-full px-2 py-2 flex justify-between items-center">
       <Button  class="bg-slate-100 border-0 text-slate-600 py-3 dark:bg-black"><i class="pi pi-angle-left text-xl dark:text-gray-500"></i></Button>
       <ThemeSwitch/>
@@ -12,7 +12,7 @@
     </div>
 
     <div class="w-full p-2 flex flex-col justify-between  bg-white rounded-t-3xl dark:bg-black" 
-      :style="{ height: deviceHeight * 0.60 + 'px' }">
+      :style="{ height: deviceHeight * 0.70 + 'px' }">
 
     <div class="w-full mt-3 px-3 flex flex-col justify-between" >
      <div class="w-full">
--- components/signup/form3.vue
+++ components/signup/form3.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="primary_color">
-        <div class="w-full p-2 primary_color" :style="{ height: deviceHeight * 0.40 + 'px' }">
+        <div class="w-full p-2 primary_color" :style="{ height: deviceHeight * 0.30 + 'px' }">
             <div class="w-full px-2 py-2 flex justify-between items-center">
                 <Button @click="back()" class="bg-slate-100 border-0 cursor-pointer text-slate-600 py-3 dark:bg-black"><i
                         class="pi pi-angle-left text-xl dark:text-gray-500"></i></Button>
@@ -12,7 +12,7 @@
         </div>
 
         <div class="w-full p-2   bg-white rounded-t-3xl dark:bg-black"
-            :style="{ height: deviceHeight * 0.60 + 'px' }">
+            :style="{ height: deviceHeight * 0.70 + 'px' }">
             <div class="w-full mt-3 px-3 " >
                 <div class="w-full">
                 <p class="text-3xl font-bold dark:text-gray-400">Add your email</p>
