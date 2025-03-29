cfd75e5ba0216bafae3377184a2b14060f5d3636
gowtham-009 <139350157+gowtham-009@users.noreply.github.com>
2025-03-29 14:19:27 +0530

    cha

--- components/NKYC_Forms/nkyclist.vue
+++ components/NKYC_Forms/nkyclist.vue
@@ -6,7 +6,7 @@
                     <ThemeSwitch @click="togglePrimaryColor" />
                 </div>
                 <div class="w-full flex justify-center mt-2">
-                    <span class="text-black mt-2" style="font-size: 3rem;">NKYC</span>
+                    <span class="text-black mt-2 dark:text-gray-400" style="font-size: 3rem;">NKYC</span>
                 </div>
             </div>
             <div class="w-full p-2 flex flex-col justify-between" 
@@ -21,53 +21,53 @@
 
                     <div class="w-full px-4 mt-2">
                         <div class="w-full flex gap-5">
-                            <div class="w-14 h-14 md:w-16 md:h-16 sm:w-20 sm:h-20 bg-blue-200  rounded-full flex justify-center items-center p-2">
+                            <div class="w-14 h-14 md:w-16 md:h-16 sm:w-20 sm:h-20 bg-blue-200 dark:bg-slate-900  rounded-full flex justify-center items-center p-2">
                                 <span><i class="pi pi-folder-open text-blue-900 text-3xl md:text-4xl"></i></span>
                             </div>
                             <div class=" p-1">
-                                <p class="text-xl font-bold text-blue-950">Complete e-KYC</p>
+                                <p class="text-xl font-bold text-blue-950 dark:text-gray-400">Complete e-KYC</p>
                                 <p class="text-md font-semibold text-gray-500">Keep your Aadhaar and PAN card handy</p>
                             </div>
                         </div>
 
                         <div class="w-full flex gap-5 mt-2">
-                            <div class="w-14 h-14 md:w-16 md:h-16 sm:w-20 sm:h-20 bg-blue-200  rounded-full flex justify-center items-center p-1">
+                            <div class="w-14 h-14 md:w-16 md:h-16 sm:w-20 sm:h-20 bg-blue-200 dark:bg-slate-900  rounded-full flex justify-center items-center p-1">
                                 <span><i class="pi pi-user text-blue-900 text-3xl" ></i></span>
                             </div>
                             <div class="p-1">
-                                <p class="text-xl font-bold text-blue-950">Set up your profile</p>
+                                <p class="text-xl font-bold text-blue-950 dark:text-gray-400">Set up your profile</p>
                                 <p class="text-md font-semibold text-gray-500">Answer a few question about yourself</p>
                             </div>
                         </div>
 
                         <div class="w-full flex gap-5 mt-2">
-                            <div class="w-14 h-14 md:w-16 md:h-16 sm:w-20 sm:h-20 bg-blue-200  rounded-full flex justify-center items-center p-1">
+                            <div class="w-14 h-14 md:w-16 md:h-16 sm:w-20 sm:h-20 bg-blue-200 dark:bg-slate-900 rounded-full flex justify-center items-center p-1">
                                 <span><i class="pi pi-credit-card text-blue-900 text-3xl" ></i></span>
                             </div>
                             <div class="p-1">
-                                <p class="text-xl font-bold text-blue-950">Link your bank a/c</p>
+                                <p class="text-xl font-bold text-blue-950 dark:text-gray-400">Link your bank a/c</p>
                                 <p class="text-md font-semibold text-gray-500">Set the primary account for your
                                     transactions</p>
                             </div>
                         </div>
 
                         <div class="w-full flex gap-5 mt-2">
-                            <div class="w-14 h-14 md:w-16 md:h-16 sm:w-20 sm:h-20 bg-blue-200  rounded-full flex justify-center items-center p-1">
+                            <div class="w-14 h-14 md:w-16 md:h-16 sm:w-20 sm:h-20 bg-blue-200 dark:bg-slate-900 rounded-full flex justify-center items-center p-1">
                                 <span><i class="pi pi-verified text-blue-900 text-3xl" ></i></span>
                             </div>
                             <div class="p-1">
-                                <p class="text-xl font-bold text-blue-950">Confirm it's you</p>
+                                <p class="text-xl font-bold text-blue-950 dark:text-gray-400">Confirm it's you</p>
                                 <p class="text-md font-semibold text-gray-500">Click a photo and submit your signature
                                 </p>
                             </div>
                         </div>
 
                         <div class="w-full flex gap-5 mt-2">
-                            <div class="w-14 h-14 md:w-16 md:h-16 sm:w-20 sm:h-20 bg-blue-200  rounded-full flex justify-center items-center p-1">
+                            <div class="w-14 h-14 md:w-16 md:h-16 sm:w-20 sm:h-20 bg-blue-200 dark:bg-slate-900 rounded-full flex justify-center items-center p-1">
                                 <span><i class="pi pi-pen-to-square text-blue-900 text-3xl"></i></span>
                             </div>
                             <div class="p-1">
-                                <p class="text-xl font-bold text-blue-950">eSign and Login</p>
+                                <p class="text-xl font-bold text-blue-950 dark:text-gray-400">eSign and Login</p>
                                 <p class="text-md font-semibold text-gray-500">Sign your application and start investing
                                 </p>
                             </div>
