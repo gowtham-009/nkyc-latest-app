54f14712e1cf31553ee4ee645b7832692eaa86b5
gowtham-009 <139350157+gowtham-009@users.noreply.github.com>
2025-03-29 14:00:29 +0530

    chh

--- components/NKYC_Forms/nkyclist.vue
+++ components/NKYC_Forms/nkyclist.vue
@@ -1,79 +1,86 @@
 <template>
     <div>
-        <div class="w-full flex flex-col justify-between p-2 h-screen dark:bg-black"
-            :class="{ 'primary_color_list': isPrimaryColor }">
-            <div class="w-full p-2" >
-                <div class="w-full flex justify-end p-1">
+        <div class="w-full dark:bg-black" :class="{ 'primary_color_list': isPrimaryColor }">
+            <div class="w-full p-2 "  :style="{ height: deviceHeight * 0.15 + 'px' }">
+                <div class="w-full flex justify-end">
                     <ThemeSwitch @click="togglePrimaryColor" />
                 </div>
-                <div class="w-full flex justify-center">
+                <div class="w-full flex justify-center mt-2">
                     <span class="text-black mt-2" style="font-size: 3rem;">NKYC</span>
                 </div>
-
-                <div class="w-full">
-                    <p class="text-4xl text-center text-blue-600 font-bold">
-                        Here's what's next
-                    </p>
-                </div>
-
-                <div class="w-full px-4 mt-2" >
-                    <div class="w-full flex gap-5">
-                        <div class="w-16 h-16 bg-blue-200  rounded-full flex justify-center items-center p-1">
-                            <span><i class="pi pi-folder-open text-blue-900" style="font-size: 1.5rem;"></i></span>
-                        </div>
-                        <div class=" p-1" >
-                            <p class="text-xl font-bold text-blue-950">Complete e-KYC</p>
-                            <p class="text-md font-semibold text-gray-500">Keep your Aadhaar and PAN card handy</p>
-                        </div>
-                    </div>
-
-                    <div class="w-full flex gap-5 mt-2">
-                        <div class="w-16 h-16 bg-blue-200  rounded-full flex justify-center items-center p-1">
-                            <span><i class="pi pi-user text-blue-900" style="font-size: 1.5rem;"></i></span>
-                        </div>
-                        <div class="p-1">
-                            <p class="text-xl font-bold text-blue-950">Set up your profile</p>
-                            <p class="text-md font-semibold text-gray-500">Answer a few question about yourself</p>
-                        </div>
-                    </div>
-
-                    <div class="w-full flex gap-5 mt-2">
-                        <div class="w-16 h-16 bg-blue-200  rounded-full flex justify-center items-center p-1">
-                            <span><i class="pi pi-credit-card text-blue-900" style="font-size: 1.5rem;"></i></span>
-                        </div>
-                        <div class="p-1" >
-                            <p class="text-xl font-bold text-blue-950">Link your bank a/c</p>
-                            <p class="text-md font-semibold text-gray-500">Set the primary account for your transactions</p>
-                        </div>
-                    </div>
-
-                    <div class="w-full flex gap-5 mt-2">
-                        <div class="w-16 h-16 bg-blue-200  rounded-full flex justify-center items-center p-1">
-                            <span><i class="pi pi-verified text-blue-900" style="font-size: 1.5rem;"></i></span>
-                        </div>
-                        <div class="p-1" >
-                            <p class="text-xl font-bold text-blue-950">Confirm it's you</p>
-                            <p class="text-md font-semibold text-gray-500">Click a photo and submit your signature</p>
-                        </div>
-                    </div>
-
-                    <div class="w-full flex gap-5 mt-2">
-                        <div class="w-16 h-16 bg-blue-200  rounded-full flex justify-center items-center p-1">
-                            <span><i class="pi pi-pen-to-square text-blue-900" style="font-size: 1.5rem;"></i></span>
-                        </div>
-                        <div class="p-1" >
-                            <p class="text-xl font-bold text-blue-950">eSign and Login</p>
-                            <p class="text-md font-semibold text-gray-500">Sign your application and start investing</p>
-                        </div>
-                    </div>
-                </div>
             </div>
-            <div class="w-full">
-                <Button type="button" label="Continue" 
-            class=" primary_color text-white w-full py-4 text-xl border-0  "
-           >
-           
-          </Button>
+            <div class="w-full p-2 flex flex-col justify-between" 
+                :style="{ height: deviceHeight * 0.85 + 'px' }">
+                <div class="w-full p-1" >
+                    <div class="w-full">
+                        <p class="text-4xl text-center text-blue-600 font-bold">
+                            Here's what's next
+                        </p>
+                    </div>
+
+
+                    <div class="w-full px-4 mt-2">
+                        <div class="w-full flex gap-5">
+                            <div class="w-16 h-16 bg-blue-200  rounded-full flex justify-center items-center p-1">
+                                <span><i class="pi pi-folder-open text-blue-900" style="font-size: 1.5rem;"></i></span>
+                            </div>
+                            <div class=" p-1">
+                                <p class="text-xl font-bold text-blue-950">Complete e-KYC</p>
+                                <p class="text-md font-semibold text-gray-500">Keep your Aadhaar and PAN card handy</p>
+                            </div>
+                        </div>
+
+                        <div class="w-full flex gap-5 mt-2">
+                            <div class="w-16 h-16 bg-blue-200  rounded-full flex justify-center items-center p-1">
+                                <span><i class="pi pi-user text-blue-900" style="font-size: 1.5rem;"></i></span>
+                            </div>
+                            <div class="p-1">
+                                <p class="text-xl font-bold text-blue-950">Set up your profile</p>
+                                <p class="text-md font-semibold text-gray-500">Answer a few question about yourself</p>
+                            </div>
+                        </div>
+
+                        <div class="w-full flex gap-5 mt-2">
+                            <div class="w-16 h-16 bg-blue-200  rounded-full flex justify-center items-center p-1">
+                                <span><i class="pi pi-credit-card text-blue-900" style="font-size: 1.5rem;"></i></span>
+                            </div>
+                            <div class="p-1">
+                                <p class="text-xl font-bold text-blue-950">Link your bank a/c</p>
+                                <p class="text-md font-semibold text-gray-500">Set the primary account for your
+                                    transactions</p>
+                            </div>
+                        </div>
+
+                        <div class="w-full flex gap-5 mt-2">
+                            <div class="w-16 h-16 bg-blue-200  rounded-full flex justify-center items-center p-1">
+                                <span><i class="pi pi-verified text-blue-900" style="font-size: 1.5rem;"></i></span>
+                            </div>
+                            <div class="p-1">
+                                <p class="text-xl font-bold text-blue-950">Confirm it's you</p>
+                                <p class="text-md font-semibold text-gray-500">Click a photo and submit your signature
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="w-full flex gap-5 mt-2">
+                            <div class="w-16 h-16 bg-blue-200  rounded-full flex justify-center items-center p-1">
+                                <span><i class="pi pi-pen-to-square text-blue-900"
+                                        style="font-size: 1.5rem;"></i></span>
+                            </div>
+                            <div class="p-1">
+                                <p class="text-xl font-bold text-blue-950">eSign and Login</p>
+                                <p class="text-md font-semibold text-gray-500">Sign your application and start investing
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-full">
+                    <Button type="button" label="Continue"
+                        class=" primary_color text-white w-full py-4 text-xl border-0  ">
+                    </Button>
+                </div>
+
             </div>
 
         </div>
@@ -89,4 +96,13 @@ const isPrimaryColor = ref(true);
 const togglePrimaryColor = () => {
     isPrimaryColor.value = !isPrimaryColor.value;
 };
+
+const deviceHeight = ref(0);
+onMounted(() => {
+    deviceHeight.value = window.innerHeight;
+    window.addEventListener('resize', () => {
+        deviceHeight.value = window.innerHeight;
+    });
+});
+
 </script>
